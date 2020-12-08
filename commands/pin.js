101f23aa9fe723f01e0d5f6b3caebab4ac93408b
Noah Too <krivahtoo@gmail.com>
2020-12-08 22:43:38 +0300

    🚀 Updated pin and unpin commands

--- commands/pin.js
+++ commands/pin.js
@@ -3,8 +3,20 @@ const Command = require('./base')
 const data = {
   name: 'pin',
   admin: true,
+  usage: '[message]',
   version: '1.0.0',
-  description: 'Pin a message in a group.'
+  description: 'Pin a message in a group.',
+  run: (bot) => (ctx) => {
+    let messageId
+    if (ctx.message.reply_to_message) {
+      messageId = ctx.message.reply_to_message.message_id
+    } else if (ctx.command.args) {
+      messageId = ctx.command.args[0]
+    } else {
+      return ctx.reply('Please reply to a message you want to pin or pass the message id')
+    }
+    return ctx.pinChatMessage(messageId)
+  }
 }
 
 module.exports = Command(data)
--- commands/unpin.js
+++ commands/unpin.js
@@ -4,7 +4,10 @@ const data = {
   name: 'unpin',
   admin: true,
   version: '1.0.0',
-  description: 'Unpin a message in a group.'
+  description: 'Unpin a message in a group.',
+  run: (bot) => (ctx) => {
+    return ctx.unpinChatMessage()
+  }
 }
 
 module.exports = Command(data)
