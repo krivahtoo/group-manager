39939678c8de0dd9261241ec12e964b5b0615ce9
Noah Too <krivahtoo@gmail.com>
2020-12-12 11:26:02 +0300

    ✨ Update admin middleware #1

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -7,11 +7,11 @@ module.exports = (bot) => async (ctx, next) => {
   if (ctx.from) {
     let admin = false
     admin = admins.includes(ctx.from.id)
-    // if (ctx.chat && ctx.chat.type === 'supergroup' && !admin) {
-    //   const status = ['administrator', 'creator']
-    //   const user = await ctx.getChatMember(ctx.from.id)
-    //   admin = status.includes(user.status)
-    // }
+    if (ctx.chat && (ctx.chat.type === 'supergroup' || ctx.chat.type === 'group') && !admin) {
+      const status = ['administrator', 'creator']
+      const user = await ctx.getChatMember(ctx.from.id)
+      admin = status.includes(user.status)
+    }
     ctx.admin = admin
     debug('User admin', ctx.admin)
   }
