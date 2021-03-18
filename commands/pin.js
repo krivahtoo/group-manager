c70159c53d2365888e1bf51f1845aef919008de8
Noah Too <krivahtoo@gmail.com>
2021-03-18 15:56:50 +0300

    Update pin command
    Closes #2

--- commands/pin.js
+++ commands/pin.js
@@ -10,7 +10,7 @@ const data = {
     let messageId
     if (ctx.message.reply_to_message) {
       messageId = ctx.message.reply_to_message.message_id
-    } else if (ctx.command.args) {
+    } else if (ctx.command.args && ctx.command.args[0]) {
       messageId = ctx.command.args[0]
     } else {
       return ctx.reply('Please reply to a message you want to pin or pass the message id')
